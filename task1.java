c7cd3dfa9c431404f6e4fd3b74f76f7dbf0dc801
Николай Комзолов <77415282+Mykola-Komzolov@users.noreply.github.com>
2021-01-22 16:44:06 +0200

    Create task1.java

---
+++ task1.java
@@ -0,0 +1,22 @@
+package company.com.Main;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner console = new Scanner(System.in);
+
+        System.out.print("Введите число: ");
+
+        int number = console.nextInt();
+
+        if(number > 7) {
+            System.out.println("Привет!");
+        }
+        else {
+            System.out.println("Ошибка!");
+        }
+    }
+}
